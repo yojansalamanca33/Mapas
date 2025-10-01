e8e5b369a9364107e65e79d6ff433d59e5f3638b
yojansalamanca33 <yogansalamanca1234@gmail.com>
2025-09-30 20:52:55 -0500

    Mapas en java

---
+++ Mapasjava/src/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author aylee
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Formularios f1 = new Formularios();
+        f1.setVisible(true);
+    }
+    
+}
